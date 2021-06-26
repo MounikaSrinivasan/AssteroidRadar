6f63d4aea70bf547c9575fe356f72e64213df934
Mounika Srinivasan <mounika_srinivasan@cable.comcast.com>
2021-06-25 20:19:16 -0400

    Added instructions for API key insertion

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "2rbgFH8AIoYaeYzn664lVXdwJyKW7lHcB0GDzU0N"
+    const val API_KEY = "" //Insert API key here
 }
